e1b34eba77d0130fa1abac10b41b662ab82a1361
andrewNEW42 <sanjino56@gmail.com>
2022-03-09 19:47:45 +0200

    init

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {MainPage} from "./modules/pages/main";
+
+function App() {
+  return (
+    <div className="App">
+      <MainPage/>
+    </div>
+  );
+}
+
+export default App;
