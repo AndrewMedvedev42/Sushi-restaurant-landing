1cea38119488998e3ca27d7ecaa79989bef3b9e3
andrewNEW42 <sanjino56@gmail.com>
2022-03-15 19:47:13 +0200

    added styles for footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,29 +1,31 @@
 import React from 'react';
+import { BsInstagram, BsTwitter } from "react-icons/bs";
+import { FaFacebookF, FaLinkedinIn } from "react-icons/fa";
 
 export const Footer:React.FC = () => {
     return (
-        <footer className="section">
+        <footer className="section footer">
             <section className="content_section">
-                <section>
-                    <article>
-                        <img src="." alt="image" />
-                        <p>Lorem ipsum dolor sit amet, 
+                <section className="main_details">
+                    <article className="colunm">
+                        <img className="footer_logo" src="." alt="image" />
+                        <p className="paragraph">Lorem ipsum dolor sit amet, 
                         consectetur adipiscing elit, 
                         sed do eiusmod tempor incididunt.</p>
-                        <div>
-                            <a href="#">Facebook</a>
-                            <a href="#">Instagram</a>
-                            <a href="#">Twitter</a>
-                            <a href="#">Linkedin</a>
+                        <div className="social_media_links">
+                            <FaFacebookF size={13} className="icon"/>
+                            <BsInstagram size={13} className="icon"/>
+                            <BsTwitter size={13} className="icon"/>
+                            <FaLinkedinIn size={13} className="icon"/>
                         </div>
                     </article>
-                    <article>
+                    <article className="colunm">
                         <h4>Opening Restaurant</h4>
                         <ul>
                             <li>item</li>
                         </ul>
                     </article>
-                    <article>
+                    <article className="colunm">
                         <h4>Contact Us</h4>
                         <ul>
                             <li>item</li>
@@ -31,7 +33,7 @@ export const Footer:React.FC = () => {
                     </article>
                 </section>
                 <section>
-                    <p>2022</p>
+                    <p className="paragraph year">2022</p>
                 </section>
             </section>
         </footer>
