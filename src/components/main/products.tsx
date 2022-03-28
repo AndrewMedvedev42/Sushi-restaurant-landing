b531ab114a4530a45e26d4eb18705893ecc5ec96
andrewNEW42 <sanjino56@gmail.com>
2022-03-28 21:09:07 +0300

    fix styles of 'add to cart button'

--- src/components/main/products.tsx
+++ src/components/main/products.tsx
@@ -12,15 +12,19 @@ export const OurProducts:React.FC = () => {
                 </header>
                 <section className="products_list">
                     {
-                        Products.products.map((item:any, index:number)=>{
-                            const {image, title, price, rating} = item
+                        Products.products.map(({image, title, price, rating}, index:number)=>{
                             return (
                                 <article className="product_item content_box content_box_shadow" key={index}>
                                     <img className="product_image" src={`${process.env.PUBLIC_URL}/${image}`} alt="" />
                                     <article className="product_details">
-                                        <h5 className="product_title">{title}</h5>
-                                        <span className="product_price">{price}</span>
-                                        <span className="product_rating">{rating}</span>
+                                        <div>
+                                            <h5 className="product_title">{title}</h5>
+                                            <button className="round_text_button">Add to cart</button>
+                                        </div>
+                                        <div>
+                                            <span className="product_rating">{rating}</span>
+                                            <span className="product_price">{price}</span>
+                                        </div>
                                     </article>
                                 </article>
                             )
